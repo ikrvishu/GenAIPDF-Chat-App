03d88619fafe0215d55015ec23efe7a8d73ff244
Kumar Vishal <kumar.vishal1307@gmail.comgit config --global user.name ikrvishugit config --global user.email kumar.vishal1307@gmail.com>
2024-07-29 19:44:54 +0530

    Streaming done!

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,10 +1,24 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
-from langchain.chains import ConversationalRetrievalChain
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 
 def build_chat(chat_args: ChatArgs):
+    retriever = build_retriever(chat_args)
+    llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
+    memory = build_memory(chat_args)
+
+    return StreamingConversationalRetrievalChain.from_llm(
+        llm=llm,
+        condense_question_llm=condense_question_llm,
+        memory=memory,
+        retriever=retriever
+    )
+
+# def build_chat(chat_args: ChatArgs):
     """
     :param chat_args: ChatArgs object containing
         conversation_id, pdf_id, metadata, and streaming flag.
@@ -16,12 +30,3 @@ def build_chat(chat_args: ChatArgs):
         chain = build_chat(chat_args)
     """
 
-    retriever = build_retriever(chat_args)
-    llm = build_llm(chat_args)
-    memory = build_memory(chat_args)
-
-    return ConversationalRetrievalChain.from_llm(
-        llm =llm,
-        memory =memory,
-        retriever =retriever
-    )
